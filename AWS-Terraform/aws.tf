c5d8aca4c80589cba80d159970f7face03819f00
Albert <albertguillaume97@gmail.com>
2025-05-06 20:14:00 +0200

    ssh modification EC2

--- AWS-Terraform/aws.tf
+++ AWS-Terraform/aws.tf
@@ -193,7 +193,7 @@ resource "aws_security_group" "tartuski_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["83.51.65.100/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
