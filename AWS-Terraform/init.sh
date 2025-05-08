482d56b298a1ac86fbd0a13628a5a58da8c1c534
Isaacva8 <isaacva8@gmail.com>
2025-05-08 21:09:18 +0200

    Update init.sh

--- AWS-Terraform/init.sh
+++ AWS-Terraform/init.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 # Actualizar e instalar paquetes necesarios
 sudo su
 apt update
 cd /root
-apt install -y python3.10-venv python3-pip pkg-config libmysqlclient-dev git
+DEBIAN_FRONTEND=noninteractive apt install -y python3.10-venv python3-pip pkg-config libmysqlclient-dev git
 
 # Clonar el proyecto
 git clone https://github.com/MunchiA/TartuskiWeb.git
@@ -17,32 +16,36 @@ echo "FLASK_SECRET_KEY=tartuski321" > /root/TartuskiWeb/.env
 python3 -m venv venv
 source venv/bin/activate
 
-# Actualizar pip e instalar Gunicorn explícitamente
+# Instalar las dependencias del proyecto
 pip install --upgrade pip
+DEBIAN_FRONTEND=noninteractive apt-get install -y  libdbus-1-dev
+DEBIAN_FRONTEND=noninteractive apt-get  install -y libglib2.0-dev
+DEBIAN_FRONTEND=noninteractive apt install -y libcairo2-dev pkg-config python3-dev
+DEBIAN_FRONTEND=noninteractive apt install -y \
+  libgirepository1.0-dev \
+  pkg-config \
+  python3-dev \
+  python3-gi \
+  python3-gi-cairo \
+  gir1.2-gtk-3.0
+DEBIAN_FRONTEND=noninteractive apt install -y libicu-dev pkg-config
+sed -i 's/^cryptography==.*$/cryptography==41.0.3/' requirements.txt
+sed -i '/^ufw==/d' requirements.txt
+sed -i '/^ubuntu-pro-client==/d' requirements.txt
+sed -i '/^command-not-found==/d' requirements.txt
+sed -i 's/^distro-info==1.7+build1$/distro-info==1.0/' requirements.txt
+sed -i '/^python-apt==2.7.7+ubuntu4/d' requirements.txt
+pip install -r requirements.txt
+# Actualizar pip e instalar Gunicorn explícitamente
 pip install gunicorn
 
-# Instalar las dependencias del proyecto
-pip install -r requirements.txt
-
-# Crear servicio systemd para Gunicorn
-cat <<EOF > /etc/systemd/system/tartuski.service
-[Unit]
-Description=Tartuski Flask Application with Gunicorn
-After=network.target
-
-[Service]
-User=root
-Group=root
-WorkingDirectory=/root/TartuskiWeb
-Environment="PATH=/root/TartuskiWeb/venv/bin"
-ExecStart=/root/TartuskiWeb/venv/bin/gunicorn --workers 3 --bind 0.0.0.0:8000 app:app
-Restart=always
-
-[Install]
-WantedBy=multi-user.target
+# Crear el archivo de configuración de Gunicorn
+cat <<EOF > gunicorn.conf.py
+bind = '0.0.0.0:8000'
+workers = 3
+timeout = 120
 EOF
 
-# Activar y arrancar el servicio
-systemctl daemon-reload
-systemctl enable tartuski
-systemctl start tartuski
+# Iniciar Gunicorn con la configuración .ini
+# Asegúrate de que 'app:app' apunte a tu aplicación Flask (módulo:app)
+gunicorn -c gunicorn.conf.py app:app &
