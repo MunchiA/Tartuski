9077f1a8b4993ee97ac019205be4cccbde49d54e
Isaacva8 <isaacva8@gmail.com>
2025-05-09 23:47:54 +0200

    Update init.sh

--- AWS-Terraform/init.sh
+++ AWS-Terraform/init.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # Actualizar e instalar paquetes necesarios
-sudo apt update
-sudo DEBIAN_FRONTEND=noninteractive apt install -y python3.10-venv python3-pip pkg-config libmysqlclient-dev git
+sudo dnf update -y
+sudo dnf install -y python3 python3-pip pkgconf git dbus-devel glib2-devel cairo-devel python3-devel gobject-introspection-devel libicu-devel dbus-daemon cairo-gobject-devel gcc-c++
 
 # Clonar el proyecto
 git clone https://github.com/MunchiA/TartuskiWeb.git
 cd TartuskiWeb
 
 # Crear archivo .env para la clave secreta de Flask
-echo "FLASK_SECRET_KEY=tartuski321" > /home/ubuntu/TartuskiWeb/.env
+echo "FLASK_SECRET_KEY=tartuski321" > .env
 
 # Crear y activar entorno virtual
 python3 -m venv venv
@@ -16,15 +16,6 @@ source venv/bin/activate
 
 # Instalar las dependencias del proyecto
 pip install --upgrade pip
-sudo DEBIAN_FRONTEND=noninteractive apt install -y libdbus-1-dev libglib2.0-dev libcairo2-dev pkg-config python3-dev
-sudo DEBIAN_FRONTEND=noninteractive apt install -y \
-  libgirepository1.0-dev \
-  pkg-config \
-  python3-dev \
-  python3-gi \
-  python3-gi-cairo \
-  gir1.2-gtk-3.0
-sudo DEBIAN_FRONTEND=noninteractive apt install -y libicu-dev pkg-config
 
 # Ajustes en requirements.txt
 sed -i 's/^cryptography==.*$/cryptography==41.0.3/' requirements.txt
@@ -43,7 +34,6 @@ bind = '0.0.0.0:8000'
 workers = 3
 timeout = 120
 EOF_CONF
-
 
 # Iniciar Gunicorn
 gunicorn -c gunicorn.conf.py app:app &
