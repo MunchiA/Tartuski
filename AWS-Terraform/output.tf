751e79b169bd435c756939540a0e96df132d554b
Isaacva8 <isaacva8@gmail.com>
2025-05-12 19:33:48 +0200

    Update output.tf

--- AWS-Terraform/output.tf
+++ AWS-Terraform/output.tf
@@ -2,3 +2,11 @@ output "web_access_url" {
   value       = "http://${aws_lb.alb.dns_name}"
   description = "La URL para acceder a la aplicación web"
 }
+output "aurora_endpoint" {
+  description = "Endpoint de la base de datos Aurora"
+  value       = aws_rds_cluster.aurora_sql_cluster_tartuski.endpoint
+}
+output "s3_bucket_name" {
+  description = "Nombre del bucket S3 de respaldo"
+  value       = aws_s3_bucket.source.id
+}
