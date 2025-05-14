187c84144d12eb12e43dfe71e6ded5c4171487a2
Isaacva8 <isaacva8@gmail.com>
2025-05-14 19:23:38 +0200

    Update output.tf

--- AWS-Terraform/output.tf
+++ AWS-Terraform/output.tf
@@ -1,12 +1,14 @@
-output "web_access_url" {
-  value       = "http://${aws_lb.alb.dns_name}"
-  description = "La URL para acceder a la aplicación web"
+output "alb_dns_name" {
+  value = aws_lb.alb.dns_name
+  description = "DNS del Load Balancer para tartuski.cat"
 }
+# Output para el endpoint de la base de datos Aurora
 output "aurora_endpoint" {
-  description = "Endpoint de la base de datos Aurora"
   value       = aws_rds_cluster.aurora_sql_cluster_tartuski.endpoint
+  description = "Endpoint del clúster Aurora para conexiones de escritura"
 }
-output "s3_bucket_name" {
-  description = "Nombre del bucket S3 de respaldo"
-  value       = aws_s3_bucket.source.id
+# Output para el nombre de dominio del bucket S3 source
+output "s3_bucket_domain_name" {
+  value       = aws_s3_bucket.source.bucket_domain_name
+  description = "Nombre de dominio del bucket S3 fuente (tartuski-backup-202527)"
 }
